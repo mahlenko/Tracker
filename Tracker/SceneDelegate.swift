401835c88e31aec7695fd1b84c7734071e3e19a6
Сергей Махленко <developer@Sergejs-MacBook-Air.local>
2023-04-01 15:06:59 +0300

    Initial commit

---
+++ Tracker/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  Tracker
+//
+//  Created by Сергей Махленко on 01.04.2023.
+//
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        if (scene as? UIWindowScene) == nil { return }
+    }
+}
---
+++ Tracker/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Сергей Махленко on 01.04.2023.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
